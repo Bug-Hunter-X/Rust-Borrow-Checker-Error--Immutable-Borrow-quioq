aed826a19bb54ecf0e63c6721e588f86973631c6
BugHunter-X <askzenai@gmail.com>
2025-02-16 22:40:05 +0100

    Add bug.rs file

---
+++ bug.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+    let first = vec[0];
+    println!("First element: {}", first);
+    // The following line will panic because vec is already borrowed
+    vec.push(3);
+}
